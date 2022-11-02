6d008158cf4d772525babb1309d0f59427b30db0
Jess <gilkes.jessica@gmail.com>
2022-11-02 14:36:19 +0000

    commiting chapter1

--- chapter1/challenges/drills/lib/2_calling_methods_with_args.rb
+++ chapter1/challenges/drills/lib/2_calling_methods_with_args.rb
@@ -1,12 +1,16 @@
-# These challenges are a bit trickier and, in some cases, will required a few lines of code.  If you start to get a little stuck, take a step back and make a plan by breaking the overall task down into small steps.
+# These challenges are a bit trickier and, in some cases, will required a few lines of code.
+# If you start to get a little stuck, take a step back and make a plan by breaking the overall task down 
+# into small steps.
 
-# TASK: define a method that returns a boolean depending on whether or not a given string starts in a lowercase 'a'.
+# TASK: define a method that returns a boolean depending on whether or not a given string
+# starts in a lowercase 'a'.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is apple
 # the method should return true
 # when the string is Apple
 # it should return false
 def starts_with_the_letter_a?(string)
+  return string.start_with?("a")
   # your code goes here
 end
 
@@ -18,6 +22,7 @@ end
 # the method should return false
 def ends_with_the_letter_a?(string)
   # your code goes here
+  return string.end_with?("a")
 end
 
 # TASK: define a method that returns a boolean depending on whether or not a given string contains the substring 'hello'.
@@ -28,14 +33,17 @@ end
 # the method should return false
 def contains_hello?(string)
   # your code goes here
+  return string.include?("hello")
 end
 
-# TASK: define a method that returns a string that replaces the substring 'hello' with the substring 'goodbye'.
+# TASK: define a method that returns a string that replaces the substring 'hello' with the substring
+# 'goodbye'.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'hello folks'
 # the method should return 'goodbye folks'
 def substitute_hello_with_goodbye(string)
   # your code goes here
+  return string.gsub("hello","goodbye")
 end
 
 # TASK: define a method that returns a string that has removed all vowels from a given string.
@@ -44,6 +52,7 @@ end
 # the method should return 'hllbl'
 def remove_all_vowels(string)
   # your code goes here
+  return string.delete("aeiou")
 end
 
 # TASK: define a method that returns a string that has removed all consonants from a given string.
@@ -52,14 +61,17 @@ end
 # the method should return 'uaaoo'
 def remove_all_consonants(string)
   # your code goes here
+  return string.delete("bcdfghjklmnpqrstvwxyz")
 end
 
-# TASK: define a method that returns a string that has removed the last half of characters from a given string.
+# TASK: define a method that returns a string that has removed the last half of characters from a
+# given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'coding'
 # the method should return 'cod'
 def first_half(string)
   # your code goes here
+  return string[0, string.length.to_f/2]
 end
 
 # TASK: define a method that returns a string that has removed the first half of characters from a given string.
@@ -68,4 +80,5 @@ end
 # the method should return 'ing'
 def second_half(string)
   # your code goes here
+  return string[string.length.to_f/2, string.length]
 end
--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,4 +3,9 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
+    if password.length >= 8 && password.match?(/[!@$%&]/)
+        return true
+    else
+        return false
+    end
 end
